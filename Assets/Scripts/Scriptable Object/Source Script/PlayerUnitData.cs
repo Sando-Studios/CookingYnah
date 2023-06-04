2ac61e4be73fe3cba652fee5d1c0659b1f2d7221
Alex Sanchez <84173270+AlexanderGregorSanchez@users.noreply.github.com>
2023-06-04 15:21:10 +0800

    Removed unnecessary libraries

--- Assets/Scripts/Scriptable Object/Source Script/PlayerUnitData.cs
+++ Assets/Scripts/Scriptable Object/Source Script/PlayerUnitData.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using static UnityEditor.Sprites.Packer;
+
 
 [CreateAssetMenu(fileName = "NewPlayerUnit", menuName = "Unit/Player")]
 public class PlayerUnitData : UnitData

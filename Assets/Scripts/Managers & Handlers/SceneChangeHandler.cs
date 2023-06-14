9181ddb2eb503c628dbcd707ade427e04e3d54d6
Alex Sanchez <84173270+AlexanderGregorSanchez@users.noreply.github.com>
2023-06-14 09:44:17 +0800

    moved ScriptableObjects to different folders / added scene changing (still needs placing player in scene tho)

---
+++ Assets/Scripts/Managers & Handlers/SceneChangeHandler.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChangeHandler : MonoBehaviour
+{
+    public string sceneName;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player" && !other.isTrigger)
+            SceneManager.LoadScene(sceneName);
+    }
+}

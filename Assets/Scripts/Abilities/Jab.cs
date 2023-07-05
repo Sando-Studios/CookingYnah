9c017b818f14dcd79f057b371e8741f54f7542bc
Go Erik <erkgo.eg@gmail.com>
2023-07-06 03:24:03 +0800

    damages and stuff idk

--- Assets/Scripts/Abilities/Jab.cs
+++ Assets/Scripts/Abilities/Jab.cs
@@ -13,6 +13,12 @@ public class Jab : MonoBehaviour
 
     [SerializeField] private uint threshold = 3;
 
+    [Range(0, 500)]
+    [SerializeField] private float range;
+
+    [SerializeField] [Min(0)] private float damage;
+    [Range(1, 100)] [SerializeField] private float damageMultiplier;
+
     internal uint internalCounter = 0;
 
     public void Attack()
@@ -32,11 +38,61 @@ public class Jab : MonoBehaviour
     private void Fast()
     {
         Debug.Log("boom");
+        
+        if (!RayCast(out var obj)) return;
+        
+        DamageHandler.ApplyDamage(obj.GetComponent<Enemy>(), Convert.ToInt32(damage));
     }
 
     private void Slow()
     {
         Debug.Log("pow");
+        
+        if (!RayCast(out var obj)) return;
+
+        var dmg = damage * damageMultiplier;
+
+        DamageHandler.ApplyDamage(obj.GetComponent<Enemy>(), Convert.ToInt32(dmg));
+    }
+
+    // TODO: Maybe have this function be common on all abilities. i.e. Put on base class; Have a utility class; etc 
+    private Vector3 GetDirection()
+    {
+        // Getting direction towards mouse
+        Vector3 startingPos = transform.position; 
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Plane plane = new Plane(Vector3.up, startingPos); 
+
+        float distance;
+        Vector3 endingPos = Vector3.zero;
+
+        if (plane.Raycast(ray, out distance))
+        {
+            endingPos = ray.GetPoint(distance);
+        }
+
+        Vector3 direction = endingPos - startingPos;
+        direction.y = 0f;
+
+        return direction.normalized;
+    }
+
+    private bool RayCast(out GameObject obj)
+    {
+        obj = null;
+        
+        Debug.DrawLine(transform.position, transform.position + GetDirection() * range, Color.blue, 1f, false);
+        
+        var ray = new Ray(transform.position, GetDirection());
+        if (!Physics.Raycast(ray, out var info, range)) return false;
+        
+        Debug.DrawLine(transform.position, info.point, Color.red, 1f, false);
+        
+        Debug.Log($"hit {info.collider.name}");
+
+        obj = info.collider.gameObject;
+
+        return true;
     }
 
     public void Update()

d82ec3e9a73311179d13762289cfe2816b966f6c
Alex Sanchez <84173270+AlexanderGregorSanchez@users.noreply.github.com>
2023-07-08 19:19:25 +0800

    Added Boss Death Check for Boss Exit / new dungeon entrance from main to D2 / Fixed an issue with navmesh on top of mesh

--- Assets/Scripts/Managers & Handlers/SceneChangeHandler.cs
+++ Assets/Scripts/Managers & Handlers/SceneChangeHandler.cs
@@ -7,10 +7,16 @@ using UnityEngine.SceneManagement;
 public class SceneChangeHandler : MonoBehaviour
 {
     public string sceneName;
+    [SerializeField] private bool canUse;
+
+    public void SetCanUse(bool isUsable)
+    {
+        canUse = isUsable;
+    }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.tag == "Player" && !other.isTrigger)
+        if (other.tag == "Player" && !other.isTrigger && canUse)
             SceneManager.LoadScene(sceneName);
     }
 }

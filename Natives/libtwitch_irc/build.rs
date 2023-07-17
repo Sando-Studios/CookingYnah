9b4f6310d0e53a0caa525820f6f2ff904fb9d4c1
Go Erik <erkgo.eg@gmail.com>
2023-07-17 23:23:38 +0800

    Experimental twitch integration / Debug mode (#13)
    * build script
    
    * basic stuff
    
    * linking
    
    * reorganize
    
    * rename folder, and removed memory leak
    
    * reduced dependencies; removed garbage collect on switch listenr and join channel
    
    * finalized the base code for the native code
    
    * resolved warnings, and removed comments
    
    * changed class names and namespaces
    
    * moved natives outside of assets
    
    * finalized interface c# to rust
    
    * added a numbered voting system
    
    * updated dependencies and rust compiler
    
    * added ui for voting system
    
    * switch locs
    
    * twitch integration switched to debug mode
    
    * formating, and added documentation
    
    ---------
    
    Co-authored-by: LauSnchzz <129297636+LauSnchzz@users.noreply.github.com>

---
+++ Natives/libtwitch_irc/build.rs
@@ -0,0 +1,18 @@
+fn main() {
+    let res = csbindgen::Builder::default()
+        .input_extern_file("src/lib.rs")
+        .csharp_dll_name("Assets/Plugins/Twitch/libtwitch_irc.dll")
+        .csharp_namespace("RawNative")
+        .csharp_class_name("RawTwitch")
+        .csharp_use_function_pointer(false)
+        //        .generate_csharp_file("TwitchRust.cs");
+        .generate_to_file(
+            "../../Assets/Plugins/libtwitch_irc.dll",
+            "../../Assets/Natives/Twitch/TwitchRust.cs",
+        );
+
+    match res {
+        Ok(_) => {}
+        Err(e) => eprintln!("Error building: {e}"),
+    }
+}

2748b6f14e23a2f4509ff93674d7e7154f03e26b
Alex Sanchez <84173270+AlexanderGregorSanchez@users.noreply.github.com>
2023-07-19 12:59:26 +0800

    Artifact Door Added to Home Base

--- Assets/Scripts/Managers & Handlers/SceneChangeHandler.cs
+++ Assets/Scripts/Managers & Handlers/SceneChangeHandler.cs
@@ -8,10 +8,17 @@ public class SceneChangeHandler : MonoBehaviour
 {
     public string sceneName;
     [SerializeField] private bool canUse;
+    [SerializeField] private GameObject blockObject;
 
+    private void Start()
+    {
+        SetCanUse(canUse);
+    }
     public void SetCanUse(bool isUsable)
     {
         canUse = isUsable;
+        if (blockObject)
+            blockObject.SetActive(!canUse);
     }
 
     private void OnTriggerEnter(Collider other)

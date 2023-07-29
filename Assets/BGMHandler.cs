4da8c031801970be5c8f4dc643ae10829ece134e
Alex Sanchez <84173270+AlexanderGregorSanchez@users.noreply.github.com>
2023-07-29 20:48:42 +0800

    Added Dungeon music / music now pauses when enter boss room (#53)
    Co-authored-by: LauSnchzz <129297636+LauSnchzz@users.noreply.github.com>

--- Assets/BGMHandler.cs
+++ Assets/BGMHandler.cs
@@ -2,10 +2,37 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class BGMHandler : MonoBehaviour
 {
     public static BGMHandler instance;
+    public AudioSource mainMenuTheme;
+    public AudioSource mainLevelTheme;
+    public AudioSource d1Theme;
+    public AudioSource d2Theme;
+    public AudioSource d3Theme;
+
+    private string currentSceneName;
+
+    private void OnEnable()
+    {
+        SceneManager.sceneLoaded += OnSceneLoaded;
+        BossRoomTrigger.OnEnterBossRoom += PlayerEnterBossRoom;
+    }
+
+    private void OnDisable()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+        BossRoomTrigger.OnEnterBossRoom -= PlayerEnterBossRoom;
+    }
+
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        currentSceneName = scene.name;
+        UpdateMusic();
+    }
 
     private void Awake()
     {
@@ -17,6 +44,59 @@ public class BGMHandler : MonoBehaviour
         else
         {
             Destroy(gameObject);
+        }
+    }
+
+    private void UpdateMusic()
+    {
+        switch (currentSceneName)
+        {
+            case "Main Level":
+                PauseAllAudio();
+                mainLevelTheme.Play();
+                break;
+            case "Dungeon 1 Level":
+                PauseAllAudio();
+                d1Theme.Play();
+                break;
+            case "Dungeon 2 Level":
+                PauseAllAudio();
+                d2Theme.Play();
+                break;
+            case "Dungeon 3 Level":
+                PauseAllAudio();
+                d3Theme.Play();
+                break;
+            case "MainMenu":
+                PauseAllAudio();
+                mainMenuTheme.Play();
+                break;
+            case "Ending Cutscene":
+                PauseAllAudio();
+                mainMenuTheme.Play();
+                break;
+        }
+    }
+
+    public void PauseAllAudio()
+    {
+        d1Theme.Pause();
+        d2Theme.Pause();
+        d3Theme.Pause();
+        mainMenuTheme.Pause();
+        mainLevelTheme.Pause();
+    }
+
+    private void PlayerEnterBossRoom(int i)
+    {
+        switch (i)
+        {
+            case 1:
+                PauseAllAudio();
+                break;
+            case -1:
+                UpdateMusic();
+                break;
         }
     }
 }
